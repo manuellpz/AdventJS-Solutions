71a6e75af10c53c8b659d65160588f062779b4ab
Manuel Lopez <manuellpz@gmail.com>
2023-12-14 19:33:50 -0700

    Solving Challenge_13

--- challenge_13.js
+++ challenge_13.js
@@ -10,47 +10,37 @@ function calculateTime(deliveries) {
   }
   let [h, m, s] = sumatoria;
 
-  if(s===0) {
-   sc = 0
+  //Seconds
+  if(s === 0) sc = 0 
+  else if(s > 0 && s < 60) sc = 60-s
+  else {
+    m++
+    sc = 60-s
   }
-  else if(s>=60) {
-   sc = 60 - s
-   m--
-  }
-
-  if(m===0) mn = 0
+  //Minutes
+  if(m === 0) mn = 0
   else if(m >= 60) {
-   mn = 60 - m
-   h--
-  }
-  else if(m < 60) mn = 60 - m
-
-  if(mn < 0){ 
-   h--
-   hr = (h+7)
-
-  }else if(h === 7){
-   hr = 0
+    h += 2
+    mn = 60 - m
   }
   else {
-   h++
-   hr = 7-h
+    h++
+    mn = 60-m
+  } 
+  //Hours
+  if(h <= 7) {
+    hr = 7 - h
+  }else {
+    hr = 8 - h
   }
 
   console.log(sumatoria, [hr, mn, sc]);
 
-  result = "0" + hr + ":";
+  result = hr >= 0 && mn<0 && h < 7 ? "-0" + Math.abs(hr) + ":" : "0" + Math.abs(hr) + ":";
   result += Math.abs(mn) < 10 ? "0" + Math.abs(mn) + ":" : Math.abs(mn) + ":";
   result += Math.abs(sc) < 10 ? "0" + Math.abs(sc) : Math.abs(sc);
 
-  return parseInt(result[1]) > 0 && parseInt(result[1]) < 7
-    ? "-" + result
-    : result;
+  return result;
 }
 
-console.log(calculateTime([
-   '00:45:00',
-   '00:45:00',
-   '00:00:30',
-   '00:00:30'
- ]));
+console.log(calculateTime(['01:00:00', '05:00:00', '00:30:00']));
